727d6092bfa959b319a6bdc637c34ddd2d2b298f
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-12-10 13:34:31 +0700

    Micro changes

--- src/unpack.ts
+++ src/unpack.ts
@@ -38,32 +38,36 @@ export function unpack<T>(
 
   return new Promise<T>((resolve, reject) => {
     form.parse(request, async (_error: any, _fields: any, _files: any) => {
-      if (_error) return reject(_error);
+      try {
+        if (_error) throw _error;
 
-      let files = _files;
-      const json = _fields[JSON_KEY];
+        let files = _files;
+        const json = _fields[JSON_KEY];
 
-      if (typeof map === 'function') {
-        files = await mapFiles(_files, map);
-      }
-
-      const data = JSON.parse(json, (key, value) => {
-        const file = files[value];
-
-        if (file) {
-          const uuid = value.split(FILE_PREFIX)[1];
-
-          if (validate(uuid)) {
-            return file;
-          } else {
-            return value;
-          }
+        if (typeof map === 'function') {
+          files = await mapFiles(_files, map);
         }
 
-        return value;
-      });
+        const data = JSON.parse(json, (key, value) => {
+          const file = files[value];
 
-      return resolve(data);
+          if (file !== undefined) {
+            const uuid = value.split(FILE_PREFIX)[1];
+
+            if (validate(uuid)) {
+              return file;
+            } else {
+              return value;
+            }
+          }
+
+          return value;
+        });
+
+        return resolve(data);
+      } catch (error) {
+        reject(error);
+      }
     });
   });
 }
